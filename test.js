12e0b61c76e09c5dfe85d622e03b61c10760bb3a
Jin Qiming <qiming.jin@nokia.com>
2015-05-07 13:47:07 +0800

    update dependency
    update dependency

--- test.js
+++ test.js
@@ -4,11 +4,11 @@ var assert = require('assert');
 var spider = new Spider();
 
 spider.route('http://www.baidu.com',function(error, $){
-  assert.equal($('#nv').text(),'新 闻　网 页　贴 吧　知 道　音 乐　图 片　视 频　地 图');
+  assert.equal($('title').text(),'百度一下，你就知道');
 });
 
 spider.route('http://login.renren.com/ajax/getEncryptKey',{json:true},function(error, json){
-  assert.equal(json.rkey,'d0cf42c2d3d337f9e5d14083f2d52cb2');
+  assert.equal(json.rkey.length,32);
 });
 
 var cookiejar = require('request').jar();
